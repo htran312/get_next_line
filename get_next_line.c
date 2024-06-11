a0a543cb8649fd7e98f59b39a467b326c63c8f3d
Huong Tran Thi Thu <htran-th@c2r6p7.hive.fi>
2024-06-11 16:54:38 +0300

    while loop condition changed

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: htran-th <htran-th@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/31 15:36:10 by htran-th          #+#    #+#             */
-/*   Updated: 2024/06/10 20:10:36 by htran-th         ###   ########.fr       */
+/*   Updated: 2024/06/11 16:36:53 by htran-th         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 static char *ft_free(char **s)
 {
-    if (s && *s)
+    if (*s)
     {
         free (*s);
         *s = NULL;
@@ -22,19 +22,25 @@ static char *ft_free(char **s)
     return (NULL);
 }
 
-static char *ft_readed(int fd, char *big_buf)
+static char *ft_read(int fd, char *big_buf)
 {
-    int readed;
+    int been_read;
     char *small_buf;
     char *tmp;
     
     small_buf = malloc((BUFFER_SIZE + 1) * sizeof(char));
     if(!small_buf)
         return (ft_free(&big_buf));
-    readed = 0;
-    while ((readed = read(fd, small_buf, BUFFER_SIZE)) > 0)
+    been_read = 1;
+    while (been_read)
     {
-        small_buf[readed] = '\0';
+        been_read = read(fd, small_buf, BUFFER_SIZE);
+        if (been_read < 0) //figure out why it work tha same wo this
+        {
+            free (small_buf);
+            return (ft_free(&big_buf));
+        }
+        small_buf[been_read] = '\0';
         tmp = big_buf;
         big_buf = ft_strjoin_gnl(big_buf, small_buf);
         if(!big_buf)
@@ -45,18 +51,13 @@ static char *ft_readed(int fd, char *big_buf)
         if (ft_strchr(big_buf, '\n'))
             break;
     }
-    if (readed < 0)
-    {
-        free (small_buf);
-        return (ft_free(&big_buf));
-    }
     free(small_buf);
     return (big_buf);
 }
 
-static char *ft_extract_readed_line(char *big_buf)
+static char *ft_extract_read_line(char *big_buf)
 {
-    char *readed_line;
+    char *read_line;
     int i;
     
     i = 0;
@@ -66,12 +67,12 @@ static char *ft_extract_readed_line(char *big_buf)
         i++;
     if (big_buf[i] == '\n')
         i++;
-    readed_line = malloc((i + 1) * sizeof(char));
-    if (!readed_line)
+    read_line = malloc((i + 1) * sizeof(char));
+    if (!read_line)
         return (NULL);
-    ft_memcpy(readed_line, big_buf, i);
-    readed_line[i] = '\0';
-    return (readed_line);    
+    ft_memcpy(read_line, big_buf, i);
+    read_line[i] = '\0';
+    return (read_line);    
 }
 static void ft_get_remainder(char **big_buf)
 {
@@ -83,13 +84,14 @@ static void ft_get_remainder(char **big_buf)
         i++;
     if ((*big_buf)[i] == '\n')
         i++;
+    /*at the EOF meaning there's no remainder, we still need to free
+    (the whole) big_buf. Check if at the current index, if big_buf is
+    null -> do the below*/
     if (!(*big_buf + i))
     {
         ft_free(big_buf);
         return ;
     }
-    /*if((*big_buf)[i] == '\0')//the EOF when there's no remainder
-        return (ft_free(big_buf));*/
     new_big_buf = ft_strdup(*big_buf + i);
     free(*big_buf);
     *big_buf = new_big_buf;
@@ -102,15 +104,11 @@ char *get_next_line(int fd)
     
     line = NULL;
     if (fd < 0 ||BUFFER_SIZE <= 0 || read(fd, NULL, 0) < 0)
-    {
-        free(big_buf);
-        big_buf = NULL;
-        return (NULL);
-    }
-    big_buf = ft_readed(fd, big_buf);
+        return (ft_free(&big_buf));
+    big_buf = ft_read(fd, big_buf);
     if (!big_buf)
         return (NULL);
-    line = ft_extract_readed_line(big_buf);
+    line = ft_extract_read_line(big_buf);
     if (!line)
         return (ft_free(&big_buf));
     ft_get_remainder(&big_buf);
